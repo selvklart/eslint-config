e730cb9596fcf1dc643b278f62e79166dd64ea17
Bruno Santos <bruno@selvklart.no>
2022-07-15 15:06:52 +0200

    feat(react-hooks): Added ruleset and preset for React Hooks and removed old React config

--- presets/react.eslintrc.js
+++ presets/react.eslintrc.js
@@ -1,8 +1,10 @@
 // React
 // Peer dependencies:
 // - eslint-plugin-react
+// - eslint-plugin-react-hooks
 export default {
 	extends: [
-		'../rules/react.eslintrc.js'
+		'../rules/react.eslintrc.js',
+		'../rules/react-hooks.eslintrc.js'
 	]
 };
---
+++ rules/react-hooks.eslintrc.js
@@ -0,0 +1,10 @@
+const error = process.env.NODE_ENV === 'production' ? 'error' : 'warn';
+
+export default {
+	// https://github.com/facebook/react/tree/main/packages/eslint-plugin-react-hooks @ v4.6.0
+	plugins: ['react-hooks'],
+	rules: {
+		'react-hooks/rules-of-hooks': error, // Checks rules of Hooks
+    	'react-hooks/exhaustive-deps': error // Checks effect dependencies
+	}
+}
